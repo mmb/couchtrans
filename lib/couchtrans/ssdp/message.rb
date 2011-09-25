2927a085723a8112391b4979c58fc57d486f93a4
Matthew M. Boedicker <matthewm@boedicker.org>
2011-09-24 22:36:19 -0400

    allow only space instead of \s

--- lib/couchtrans/ssdp/message.rb
+++ lib/couchtrans/ssdp/message.rb
@@ -4,7 +4,7 @@ module Couchtrans
 
     Eol = "\r\n"
     StartLineRe = %r{^(?:(NOTIFY|M-SEARCH) \* HTTP/1.1|HTTP/1.1 200 (OK))$}i
-    HeaderRe = %r{^([a-z.\-]+):\s*(.*)$}i
+    HeaderRe = %r{^([a-z.\-]+): *(.*)$}i
 
     class Message
 

4d7ede5986df4e34c2b7437f4d11119924b9e2be
Matthew M. Boedicker <matthewm@boedicker.org>
2012-03-30 06:40:28 -0400

    send correct response for content directory search

--- lib/couchtrans/ssdp/search_message_handler.rb
+++ lib/couchtrans/ssdp/search_message_handler.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module Couchtrans
 
   module Ssdp
@@ -6,8 +8,29 @@ module Couchtrans
       include Logged
 
       def handle(message, sender)
-        if message.meth == 'M-SEARCH'
-          log.debug 'received search message'
+        if message.meth == 'M-SEARCH' and
+          message.headers['st'] =~ /:ContentDirectory:1$/
+
+          response = Message.new
+          response.meth = 'OK'
+          response.headers = {
+            :'cache-control' => 'max-age=1800',
+            :date => Time.now.httpdate,
+            :ext => '',
+            :location => 'http://192.168.1.3:4567/',
+            :server => SERVER_HEADER,
+            :st => 'urn:schemas-upnp-org:service:ContentDirectory:1',
+            :usn => "uuid:#{Server::Uuid}::urn:schemas-upnp-org:service:ContentDirectory:1",
+          }
+
+          log.debug("sending response\n#{response}")
+          begin
+            socket = UDPSocket.open
+            socket.send response.to_s, 0, sender[3], sender[1]
+          ensure
+            socket.close 
+          end
+
         end
 
       end
